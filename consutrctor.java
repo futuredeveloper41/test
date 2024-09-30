1772f0d4f706d57d2ea6b9a9cf48e5228451d35d
futuredeveloper41 <mj10283@naver.com>
2024-09-30 20:40:54 +0900

    second
    sdffssdfsdfsdfsfsdf

---
+++ consutrctor.java
@@ -0,0 +1,36 @@
+package ininheritance;
+
+// 생성자가 있는 부모 클래스
+class cal3{
+	public int v1;
+	public int v2;
+	public cal3(int v1, int v2) {
+		this.v1 = v1;
+		this.v2 = v2;
+	}
+	
+	public int sum() {
+		return this.v1+this.v2;
+	}
+	
+}
+
+class cal4 extends cal3{
+
+	// super() : 생성자가 있는 클래스 상속받았다면, 자식 클래스도 생성자를 만들어서 만드시 부모 클래스를 호출해야 한다.
+    public cal4(int v1, int v2) {
+    	super(v1, v2);
+	}
+	
+	public int minus() {return this.v1-this.v2;}
+}
+
+public class consutrctor {
+	public static void main(String[] args) {
+		cal3 df = new cal3(2,1);
+		cal4 df1 = new cal4(2,1);
+		System.out.println(df1.sum());
+		System.out.println(df1.minus());
+	}
+
+}
